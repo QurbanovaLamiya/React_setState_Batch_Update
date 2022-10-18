f24527d14fa02a8a2d4206d6eb897a45e8ded986
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-10-18 23:49:41 +0400

    Update folder structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Component, Fragment } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1></h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
